9aa340815c7e3c5a96994e9a8e5ca2f9acf1e521
Heinrich Grobler <heinrich.grobler@entelect.co.za>
2024-03-12 09:16:58 +0200

    changed the color

--- src/ProductComponent/ProductComponent.tsx
+++ src/ProductComponent/ProductComponent.tsx
@@ -3,6 +3,7 @@ import ProductData from "./ProductData";
 
 function ProductComponent(props: ProductData) {
     return <div data-testid="productTile" key={"product-" + props.id} id={props.id} className="productTile">
+        <div className="productName">{props.id}</div>
         <div className="productName">{props.name}</div>
         <div className="productDescription">{props.description}</div>
     </div>;
